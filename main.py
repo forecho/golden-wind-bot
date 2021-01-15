e939f6ba0ad402d6a42a526537d815cdac4a5775
forecho <caizhenghai@gmail.com>
2021-01-15 22:58:14 +0800

    test

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def send_message(text):
     """
     telegram = helper.helper.config('telegram')
     bot = telepot.Bot(telegram['token'])
-    print(bot.getMe())
+    print(bot.getUpdates())
     # bot.sendMessage(telegram['chat_id'], text, 'Markdown')
 
 
